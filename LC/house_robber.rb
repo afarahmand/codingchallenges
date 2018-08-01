361264be654e07948c6597e4ca5170e26196e953
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-08-01 13:36:39 -0700

    Complete LC house robber (uses DP)

--- LC/house_robber.rb
+++ LC/house_robber.rb
@@ -1,18 +1,19 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def rob(nums)
-  if nums.length < 4
-    return 0 if nums.length == 0
-    return nums[0] if nums.length == 1
-    return nums.max if nums.length == 2
-    return [nums[0] + nums[2], nums[1]].max if nums.length == 3
+  return 0 if nums.length == 0
+  return nums[0] if nums.length == 1
+  return nums.max if nums.length == 2
+
+  # Use dynamic programming
+  dp = {}
+
+  dp[0] = nums[0]
+  dp[1] = [dp[0], nums[1]].max
+
+  nums[2..-1].each_with_index do |num, idx|
+    dp[idx+2] = [nums[idx+2] + dp[idx], dp[idx+1]].max
   end
 
-  # Choose the 0th house, then recursively call this function to choose best combination of remaining houses
-  first_element = nums[0] + rob(nums[2..-1])
-
-  # Choose the 1st house, then use recursion to make best decision on remaining houses
-  second_element = nums[1] + rob(nums[3..-1])
-
-  return [first_element, second_element].max
+  dp[nums.length-1]
 end
