b87c4f82e6040fa24b39e115c278c0311743a470
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-09-11 15:56:20 -0700

    Input files correctly parsed

--- challenge1/create_report.rb
+++ challenge1/create_report.rb
@@ -29,7 +29,32 @@ def read_file_source1(path_infile)
   result
 end
 
-def parse
+def process_action(raw_action)
+  temp = raw_action.delete("\"[] ").split(",")
+  action = {
+    "source": temp[0].split(":")[0],
+    "value": temp[0].split(":")[1].to_i,
+    "action_type": temp[1].split(":")[1]
+  }
+
+  action
+end
+
+# Parse out each individual action per entry from source2
+def process_raw_entry_part2(raw_entry)
+  raw_action_string = ""
+  actions = []
+
+  beg_idx = 2
+  fin_idx = raw_entry.index("}")
+  while beg_idx
+    raw_action_string = raw_entry[(beg_idx+1)..(fin_idx-1)]
+    beg_idx = raw_entry.index("{", fin_idx)
+    fin_idx = raw_entry.index("}", beg_idx) if beg_idx
+    actions << process_action(raw_action_string)
+  end
+
+  actions
 end
 
 def read_file_source2(path_infile)
@@ -37,33 +62,20 @@ def read_file_source2(path_infile)
   result = []
 
   raw_lines.each_with_index do |line, idx|
-    raw_entry = line.chomp.split(",")
+    beg_index_of_part2 = line.index("\"[{")
+    raw_entry_part1 = line.chomp[0..(beg_index_of_part2 - 2)].split(",")
+    raw_entry_part2 = line.chomp[beg_index_of_part2..(-1)]
 
-    # p raw_entry
-    p raw_entry[3]
-    # p raw_entry[4..-1]
-    # raw_entry[4..-1].each do |element|
-    #   print element.delete("\"").length.to_s + " | "
-    #   p element.delete("\"").delete("[").delete("{").delete(" ")
-    #   sleep(5)
-    # end
-    # p "---"
-
-    # raw_entry = line.chomp.split(",")
-    # raw_field1 = raw_entry[1].split("_")
     entry = {}
-    #
+
     entry["id"] = idx
-    entry["campaign_id"] = raw_entry[0]
-    entry["ad_type"] = raw_entry[1]
-    entry["date"] = raw_entry[2]
-    entry["spend"] = raw_entry[3]
-    # entry["state"] = raw_field1[0]
-    # entry["hair_color"] = raw_field1[1]
-    # entry["age_range"] = raw_field1[2]
-    # entry["impressions"] = raw_entry[2].to_i
-    #
-    # result << entry
+    entry["campaign_id"] = raw_entry_part1[0]
+    entry["ad_type"] = raw_entry_part1[1]
+    entry["date"] = raw_entry_part1[2]
+    entry["spend"] = raw_entry_part1[3].to_i
+    entry["actions"] = process_raw_entry_part2(raw_entry_part2)
+
+    result << entry
   end
 
   result
@@ -71,14 +83,8 @@ end
 
 
 def create_report
-  # source1 = read_file_source1("source1.csv")
-
+  source1 = read_file_source1("source1.csv")
   source2 = read_file_source2("source2.csv")
-  # i = 0
-  # while i < 5
-  #   p source2[i]
-  #   i+=1
-  # end
 
 
 end
