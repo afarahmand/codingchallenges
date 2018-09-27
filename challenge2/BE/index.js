eff2cc6ac1f4fbc7a3c95f825705d544bc1d96d7
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-09-27 00:09:25 -0700

    Complete BE portion of challenge2

--- challenge2/BE/index.js
+++ challenge2/BE/index.js
@@ -1,73 +1,94 @@
-// 1 - Grab zip codes via Redis
-// 2 - Grab weather info via DarkSky API
-// 3 - Compute AVG
-// 4 - Output to console
-// 5 - POST in json format
+// 1 - Grab 3 zip codes via 3 given Redis keys at the provided ip address
+// 2 - Convert zip codes into latitude (lat) and longitude (lon) using
+//   zipcodes npm package
+// 3 - Grab daily highTemperature and lowTemperature for each lat/lon
+//   via DarkSky API
+// 4 - Compute average temperature for each location, then composite
+//   avg of all locations
+// 5 - Output composite avg using console.log
+// 6 - POST in json format to given url
 
-const dsSecretKey = "redacted secret key";
+const getAverageTemperature = dsApiResponse => {
+  let respDaily = JSON.parse(dsApiResponse).daily.data[0];
+  return ((respDaily.temperatureHigh+respDaily.temperatureLow)/2);
+};
+
+const dsSecretKey = "REDACTED";
 const redis = require('redis');
 const request = require('request');
 const zipcodes = require('zipcodes');
 
-// 1 - Grab zip codes via Redis
+// 1 - Grab zip codes via Redis at g
 // By default redis.createClient() will use 127.0.0.1 and port 6379
-const redisClient = redis.createClient(6379, '<redacted IP address');
+const redisClient = redis.createClient(6379, 'REDACTED ip address');
 
 redisClient.on('connect', function() {
-  redisClient.auth('redacted');
-  console.log('Redis client connected');
-  let zipCodesArr = [];
+  // Auth must be the first thing done after connection (else errors/warnings)
+  redisClient.auth('REDACTED key');
+  console.log('Redis client successfully connected!!!');
 
-  redisClient.get('zipcode1', function(err1, dbZC1){
-    zipCodesArr.push(dbZC1);
-    redisClient.get('zipcode2', function (err2, dbZC2) {
-      zipCodesArr.push(dbZC2);
-      redisClient.get('zipcode3', function (err3, dbZC3) {
-        zipCodesArr.push(dbZC3);
+  // Get the values for all 3 keys at once
+  redisClient.mget(['zipcode1', 'zipcode2', 'zipcode3'], function(err, zcArr){
 
-        // Convert zipcodes to long/lat
-        let latLon = [];
-        let zcObj = {};
+    let latLons = [];
+    let zcObj = {};  // a temp variable; stores output of zipcode module
 
-        for (let zipCode of zipCodesArr) {
-          zcObj = zipcodes.lookup(zipCode);
-          console.log([zcObj.latitude, zcObj.longitude]);
-          latLon.push([zcObj.latitude, zcObj.longitude]);
-        }
+    for (let zipCode of zcArr) {
+      zcObj = zipcodes.lookup(zipCode);
+      latLons.push([zcObj.latitude, zcObj.longitude]);
+    }
 
-        // 2 - Grab weather info via DarkSky API
+    let urls = [];   // stores 3 urls, one for each location
+    // param str that reduces the amount of data accessed via the DarkSky API
+    const base = "https://api.darksky.net/forecast/";
+    const paramString = "exclude=currently,minutely,hourly,alerts,flags";
 
-        const excluded = "exclude=currently,minutely,hourly,alerts,flags";
+    for (let latLon of latLons) {
+      urls.push(
+        `${base}${dsSecretKey}/${latLon[0]},${latLon[1]}?${paramString}`
+      );
+    }
 
-        const url1 = `https://api.darksky.net/forecast/${dsSecretKey}/${latLon[0][0]},${latLon[0][1]}?${excluded}`;
-        const url2 = `https://api.darksky.net/forecast/${dsSecretKey}/${latLon[1][0]},${latLon[1][1]}?${excluded}`;
-        const url3 = `https://api.darksky.net/forecast/${dsSecretKey}/${latLon[2][0]},${latLon[2][1]}?${excluded}`;
-        let runningAvg = 0;
+    let sumOfAvgs = 0;
+    request.get(urls[0]).on('data', function(apiResponse0){
+      sumOfAvgs+=getAverageTemperature(apiResponse0);
 
-        request.get(url1).on('data', function(res1){
-          let respDaily = JSON.parse(res1).daily.data[0];
-          let avgTemp = (respDaily.temperatureHigh+respDaily.temperatureLow)/2;
-          runningAvg+=avgTemp;
+      request.get(urls[1]).on('data', function(apiResponse1){
+        sumOfAvgs+=getAverageTemperature(apiResponse1);
 
-          request.get(url2).on('data', function(res2){
-            respDaily = JSON.parse(res2).daily.data[0];
-            avgTemp = (respDaily.temperatureHigh+respDaily.temperatureLow)/2;
-            runningAvg+=avgTemp;
+        request.get(urls[2]).on('data', function(apiResponse2){
+          sumOfAvgs+=getAverageTemperature(apiResponse2);
 
-            request.get(url3).on('data', function(res3){
-              respDaily = JSON.parse(res3).daily.data[0];
-              avgTemp = (respDaily.temperatureHigh+respDaily.temperatureLow)/2;
-              runningAvg+=avgTemp;
+          // 3 - Compute composite AVG
+          let compositeAvg = sumOfAvgs/3;
+          compositeAvg = Math.round(compositeAvg*1000)/1000;
 
-              // 3 - Compute AVG
-              runningAvg/=3;
-              runningAvg = Math.round(runningAvg*1000)/1000;
+          // 4 - Output to console
+          console.log("The composite average of the average daily",
+          "temperature of each of the 3 zip codes is",
+          `${compositeAvg} degrees.`);
 
-              // 4 - Output to console
-              console.log(`The composite average of the average daily temperature of the 3 zip codes is ${runningAvg}`);
+          // 5 - POST in json format
 
-              // 5 - POST in json format
-            });
+          const url1 = 'REDACTED url';
+          const accessToken = "Bearer REDACTED access token";
+
+          request.post({
+            url: url1,
+            json: true,
+            headers: {
+              'Authorization': accessToken,
+              'Content-type': 'application/json'
+            },
+            body: {
+              averageTemperature: compositeAvg
+            }
+          }, function(err4, dbPost) {
+            if (err4) {
+              console.log("Error posting to endpoint.");
+            } else {
+              console.log("Successfully posted!!!");
+            }
           });
         });
       });
